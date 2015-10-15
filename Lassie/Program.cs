e7d536cc25b08a9b9ab47123c369b69912f37631
Will Pearson <pearswj@gmail.com>
2015-10-15 16:28:04 +0100

    grab credentials from wincred
    falls back to lassie.json
    will never ask for user/pass as waiting for user input could cripple debugging

--- Lassie/Program.cs
+++ Lassie/Program.cs
@@ -4,12 +4,14 @@ using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using CredentialManagement;
 
 namespace Lassie
 {
@@ -31,123 +33,79 @@ namespace Lassie
                 return 3;
             }
 
-            var platform = WhichPlatform();
-            string configFile, configDir;
-
-            switch (platform)
-            {
-                case Platform.Mac:
-                    // do something
-                    // .mcneel
-                    var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-                    configDir = Path.Combine(home, ".mcneel");
-                    Directory.CreateDirectory(configDir);
-                    configFile = Path.Combine(configDir, "lassie.json");
-                    break;
-                case Platform.Windows:
-                    // do something else
-                    // %appdata%
-                    configDir = Path.Combine(
-                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-                        "McNeel");
-                    Directory.CreateDirectory(configDir);
-                    configFile = Path.Combine(configDir, "lassie.json");
-                    break;
-                default:
-                    // only supports windows and mac os x
-                    Console.WriteLine("Lassie supports Windows and Mac OS X only. Sorry.");
-                    return 0;
-            }
-
-            // check for stored credentials (and store credentials)
-
-            string token;
-            if (File.Exists(configFile))
-            {
-                var config = JsonConvert.DeserializeObject<AuthorizationsResponse>(File.ReadAllText(configFile));
-                token = config.Token;
-                // TODO: reset if >month since last updated
-            }
-            else
-            {
-
-                Console.WriteLine("No credentials found. Please enter your GitHub username and password.");
-                Console.WriteLine("I promise not to do anything with these details except exchange them for a \"personal access token\" which will be used to authorization.\n");
-
-                // get the username
-
-                Console.Write("Username: ");
-                string user = Console.ReadLine();
-
-                // get the pasword
-                // http://stackoverflow.com/questions/3404421/password-masking-console-application
-
-                Console.Write("Password: ");
-                //string pass = Console.ReadLine();
-                string pass = "";
-                ConsoleKeyInfo key;
-
-                do
-                {
-                    key = Console.ReadKey(true);
-
-                    // Backspace Should Not Work
-                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
-                    {
-                        pass += key.KeyChar;
-                        Console.Write("*");
-                    }
-                    else
-                    {
-                        if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
-                        {
-                            pass = pass.Substring(0, (pass.Length - 1));
-                            Console.Write("\b \b");
-                        }
-                    }
-                }
-                // Stops Receving Keys Once Enter is Pressed
-                while (key.Key != ConsoleKey.Enter);
-
-                Console.WriteLine();
-                //Console.WriteLine("The Password You entered is : " + pass);
-
-                // https://developer.github.com/v3/oauth_authorizations/#create-a-new-authorization
-                AuthorizationsResponse response;
-                try
-                {
-                    response = Foo(user, pass).Result; // I know I'm not using async properly...
-                    token = response.Token;
-                }
-                catch (Exception e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Error.WriteLine("Something went wrong during authentication ({0}).", e.InnerException.Message);
-                    Console.ResetColor();
-                    return 1;
-                }
-
-                File.WriteAllText(configFile, JsonConvert.SerializeObject(response, Formatting.Indented));
-
-                Console.WriteLine("\nYour personal access token has been stored in {0}. You won't be asked for your password again. You can revoke this token at any time by going to https://github.com/settings/tokens.\n", configFile);
-            }
-
-            // now it begins
+            // get args
 
             string repo = args[0];
             string path = args[1];
             string commitish = args[2];
             string outFilename = args[3];
 
+            // check platform
+            Platform platform = WhichPlatform();
+
+            // check for stored credentials
+
+            string token = null, user = null, pass = null;
+
+            // if windows, let's try to get the username/password from the credential manager
+            // requires the user to have setup `git config --global credential.helper wincred`
+            if (platform == Platform.Windows)
+            {
+                var creds = new CredentialSet("git:*"); // wild
+                creds.Load();
+                var credential = creds.FirstOrDefault(c => c.Target.EndsWith("github.com"));
+                if (credential != null)
+                {
+                    //Credential credential = new Credential { Target = "git:https://<username>@github.com", Type = CredentialType.Generic };
+                    credential.Load();
+                    user = credential.Username;
+                    pass = credential.Password;
+                }
+            }
+
+            // if we didn't manage to get credentials from windows' credential manager, try legacy lassie.json config file
+            if (user == null || pass == null)
+            {
+                string configFile = WhereConfig(platform);
+
+                if (File.Exists(configFile))
+                {
+                    var config = JsonConvert.DeserializeObject<Config>(File.ReadAllText(configFile));
+                    token = config.Token;
+                    // TODO: reset if >month since last updated
+                }
+                else
+                {
+                    Console.Error.WriteLine("Couldn't find any github credentials on your system. Looks like Timmy's spending the night in the well.");
+                    return 5;
+                }
+            }
+
+            // now it begins
+
+            
+            string contents;
             try
             {
-                string fileContents = Bar(repo, path, commitish, token).Result;
+                // fetch the file contents
+                if (token != null)
+                {
+                    contents = Fetch(repo, path, commitish, token);
+                }
+                else
+                {
+                    contents = Fetch(repo, path, commitish, user, pass);
+                }
+
+                // normalise line endings for windows
                 if (platform == Platform.Windows)
                 {
-                    string normalised = Regex.Replace(fileContents, @"\r\n|\n\r|\n|\r", "\r\n");
-                    fileContents = normalised;
+                    string normalised = Regex.Replace(contents, @"\r\n|\n\r|\n|\r", "\r\n");
+                    contents = normalised;
                 }
-                File.WriteAllText(outFilename, fileContents);
+
+                // write
+                File.WriteAllText(outFilename, contents);
             }
             catch (Exception e)
             {
@@ -162,79 +120,11 @@ namespace Lassie
             return 0;
         }
 
-        public static async Task<AuthorizationsResponse> Foo(string user, string pass)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri("https://api.github.com");
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(
-                    new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-                client.DefaultRequestHeaders.Add("User-Agent", "Lassie");
-                var byteArray = Encoding.ASCII.GetBytes(user + ":" + pass);
-                client.DefaultRequestHeaders.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
-
-                var content = new AuthorizationsContent();
-                content.
-                Scopes = new string[] { "repo" };
-                string profile = System.Environment.UserName;
-                string hostname = System.Environment.MachineName;
-                content.Note = string.Format("lassie for {0}@{1}", profile, hostname);
-
-                var contentString = JsonConvert.SerializeObject(content);
-                var stringContent = new StringContent(contentString);
-
-                var response = await client.PostAsync("authorizations", stringContent);
-
-                // 2FA
-                // https://developer.github.com/v3/auth/#working-with-two-factor-authentication
-                if (response.Headers.Contains("X-GitHub-OTP"))
-                {
-                    Console.WriteLine("Hats off to you for having 2-factor-authentication enabled!");
-                    Console.Write("OTP: ");
-                    string otp = Console.ReadLine();
-                    client.DefaultRequestHeaders.Add("X-GitHub-OTP", otp);
-                    var stringContent2 = new StringContent(contentString);
-                    response = await client.PostAsync("authorizations", stringContent2);
-                }
-
-                string json;
-
-                if (!response.IsSuccessStatusCode)
-                {
-                    try
-                    {
-                        json = await response.Content.ReadAsStringAsync();
-                        var obj = JsonConvert.DeserializeObject(json);
-                        string formatted = JsonConvert.SerializeObject(obj, Formatting.Indented);
-                        Console.WriteLine(formatted);
-                        var data = JObject.Parse(json);
-                        //Console.ForegroundColor = ConsoleColor.Red;
-                        //Console.Error.WriteLine(data["message"]);
-                        //Console.ResetColor();
-                        throw new InvalidOperationException(data["message"].ToString());
-                    }
-                    catch (JsonException e)
-                    {
-                        throw new InvalidOperationException("Couldn't parse JSON error message.", e);
-                    }
-                }
-
-                json = response.Content.ReadAsStringAsync().Result;
-                return JsonConvert.DeserializeObject<AuthorizationsResponse>(json);
-            }
-        }
-
-        public class AuthorizationsContent
-        {
-            [JsonProperty("scopes")]
-            public string[] Scopes;
-            [JsonProperty("note")]
-            public string Note;
-        }
-
-        public class AuthorizationsResponse
+        
+        /// <summary>
+        /// Used for (de)serialisation of lassie.json
+        /// </summary>
+        public class Config
         {
             [JsonProperty("token")]
             public string Token;
@@ -244,7 +134,20 @@ namespace Lassie
             public string UpdatedAt;
         }
 
-        public static async Task<string> Bar(string repo, string path, string commitish, string token)
+        public static string Fetch(string repo, string path, string commitish, string token)
+        {
+            var auth = new AuthenticationHeaderValue("token", token);
+            return Fetch(repo, path, commitish, auth);
+        }
+
+        public static string Fetch(string repo, string path, string commitish, string user, string pass)
+        {
+            var byteArray = Encoding.ASCII.GetBytes(user + ":" + pass);
+            var auth = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+            return Fetch(repo, path, commitish, auth);
+        }
+
+        private static string Fetch(string repo, string path, string commitish, AuthenticationHeaderValue auth)
         {
             using (var client = new HttpClient())
             {
@@ -253,46 +156,39 @@ namespace Lassie
                 client.DefaultRequestHeaders.Accept.Add(
                     new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
                 client.DefaultRequestHeaders.Add("User-Agent", "Lassie");
-                //var byteArray = Encoding.ASCII.GetBytes(token);
-                client.DefaultRequestHeaders.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue("token", token);
+                client.DefaultRequestHeaders.Authorization = auth;
 
                 var endpoint = string.Format("repos/{0}/contents/{1}?ref={2}", repo, path, commitish);
 
-                var response = await client.GetAsync(endpoint);
-                string json;
+                var response = client.GetAsync(endpoint).Result;
 
-                if (!response.IsSuccessStatusCode)
+                // get all the jsons
+                string json = response.Content.ReadAsStringAsync().Result;
+                JObject data;
+                try
                 {
-                    try
-                    {
-
-                        json = await response.Content.ReadAsStringAsync();
-                        var obj = JsonConvert.DeserializeObject(json);
-                        string formatted = JsonConvert.SerializeObject(obj, Formatting.Indented);
-                        Console.WriteLine(formatted);
-                        var data = JObject.Parse(json);
-                        //Console.ForegroundColor = ConsoleColor.Red;
-                        //Console.Error.WriteLine(data["message"]);
-                        //Console.ResetColor();
-                        throw new InvalidOperationException(data["message"].ToString());
-                    }
-                    catch (JsonException e)
-                    {
-                        throw new InvalidOperationException("Couldn't parse JSON error message.", e);
-                    }
+                    data = JObject.Parse(json);
+                }
+                catch (JsonException e)
+                {
+                    throw new InvalidOperationException("Couldn't parse GitHub's error message.", e);
                 }
 
-                json = response.Content.ReadAsStringAsync().Result;
-                //var obj2 = JsonConvert.DeserializeObject<Object>(json);
-                var r = JObject.Parse(json);
-                string content = r["content"].ToString();
+                // bad response from github
+                if (!response.IsSuccessStatusCode)
+                {
+                    // throw a tantrum and send github's error message back up the chain    
+                    throw new InvalidOperationException(data["message"].ToString());
+                }
+
+                string content = data["content"].ToString();
                 // TODO: check if "type" is "file"
                 return Encoding.UTF8.GetString(Convert.FromBase64String(content));
             }
-
-            //Console.WriteLine ("Hello World!");
         }
+
+        // helper methods to correctly determine platform
+        // http://stackoverflow.com/questions/10138040/how-to-detect-properly-windows-linux-mac-operating-systems
 
         public enum Platform
         {
@@ -321,6 +217,33 @@ namespace Lassie
 
                 default:
                     return Platform.Windows;
+            }
+        }
+
+        // where should we look for the config file?
+        public static string WhereConfig(Platform platform)
+        {
+            string configDir;
+
+            switch (platform)
+            {
+                case Platform.Mac:
+                case Platform.Linux:
+                    // do something
+                    // .mcneel
+                    var home = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                    configDir = Path.Combine(home, ".mcneel");
+                    Directory.CreateDirectory(configDir);
+                    return Path.Combine(configDir, "lassie.json");
+                case Platform.Windows:
+                default:
+                    // do something else
+                    // %appdata%
+                    configDir = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                        "McNeel");
+                    Directory.CreateDirectory(configDir);
+                    return Path.Combine(configDir, "lassie.json");
             }
         }
     }
