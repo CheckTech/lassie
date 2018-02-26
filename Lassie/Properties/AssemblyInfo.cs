b5e075e90404e79bd99e6cd67a5710487bfec7b9
Will Pearson <will@mcneel.com>
2018-02-26 14:31:36 +0000

    Bump version to 0.2

--- Lassie/Properties/AssemblyInfo.cs
+++ Lassie/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Robert McNeel and Associates")]
 [assembly: AssemblyProduct ("Lassie")]
-[assembly: AssemblyCopyright ("Copyright © 2015 Robert McNeel and Associates")]
+[assembly: AssemblyCopyright ("Copyright © 2015, 2018 Robert McNeel and Associates")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("0.1.*")]
+[assembly: AssemblyVersion ("0.2.*")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
